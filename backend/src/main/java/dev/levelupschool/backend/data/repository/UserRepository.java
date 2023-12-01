2a147cd11376f1ab577f21f57c610dfe0c44b1f4
Kabir Yusuf <kyaa@Kabirs-MacBook-Pro.local>
2023-12-01 08:12:12 +0100

    feat: implemented many-many r/shp between user and articles for bookmarked article, all unit and integration test passing and database base migration script written to create a table for the r/shp

--- backend/src/main/java/dev/levelupschool/backend/data/repository/UserRepository.java
+++ backend/src/main/java/dev/levelupschool/backend/data/repository/UserRepository.java
@@ -16,8 +16,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.followers WHERE u.id = :userId")
     Optional<User> findByIdWithFollowers(Long userId);
 <<<<<<< HEAD
+<<<<<<< HEAD
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.bookmarkedArticles WHERE u.id = :userId")
     Optional<User> findByIdWithBookmarkedArticles(Long userId);
 =======
 >>>>>>> 154319b (feat: Implemented many-many r/shp for users and followers with all unit and integration test passing)
+=======
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.bookmarkedArticles WHERE u.id = :userId")
+    Optional<User> findByIdWithBookmarkedArticles(Long userId);
+>>>>>>> 86c245a (feat: implemented many-many r/shp between user and articles for bookmarked article, all unit and integration test passing and database base migration script written to create a table for the r/shp)
 }
