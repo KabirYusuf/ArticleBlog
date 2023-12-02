558d065a53b33d7a1388ba09735034bdd2e0e432
Kabir Yusuf <kyaa@Kabirs-MacBook-Pro.local>
2023-12-02 09:24:24 +0100

    fix: converted nav to component

--- frontend/src/components/layouts/Header.vue
+++ frontend/src/components/layouts/Header.vue
@@ -1,24 +1,9 @@
 <template>
   <header class="header">
     <HambugerMenu />
-    <div class="header__overlay">
-      <nav class="nav">
-        <div class="nav__logoContainer">
-          <a class="nav__logoLink" href="#">RUNO</a>
-        </div>
-        <ul class="nav__list">
-          <li class="nav__listItem nav_listItem--pushRight">
-            <router-link to="/" class="nav__listLink">Home</router-link>
-          </li>
-          <li class="nav__listItem"><a class="nav__listLink" href="#">About</a></li>
-          <li class="nav__listItem"><a class="nav__listLink" href="#">Article</a></li>
-          <li class="nav__listItem" @click="modalStore.openModal('login')"><a class="nav__listLink" href="#">Sign in</a>
-          </li>
-          <li class="nav__listItem" @click="modalStore.openModal('register')"><a class="nav__listLink"
-              href="#">Register</a></li>
-        </ul>
-      </nav>
-    </div>
+    
+      <NavBar/>
+    
 
     <div class="headerText__container" v-if="!isCenter">
       <div class="headerText__inner">
@@ -63,6 +48,7 @@ import { useModalStore } from "@/store/modalStore";
 import RegisterModal from "../modals/RegisterModal.vue"
 import Modal from '../modals/Modal.vue'
 import HambugerMenu from '../hamburger/HambugerMenu.vue'
+import NavBar from './NavBar.vue'
 
 defineProps(
   ['card', 'isCenter']
---
+++ frontend/src/components/layouts/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+
+<div class="header__overlay">
+    <nav class="nav">
+        <div class="nav__logoContainer">
+          <a class="nav__logoLink" href="#">RUNO</a>
+        </div>
+        <ul class="nav__list">
+          <li class="nav__listItem nav_listItem--pushRight">
+            <router-link to="/" class="nav__listLink">Home</router-link>
+          </li>
+          <li class="nav__listItem"><a class="nav__listLink" href="#">About</a></li>
+          <li class="nav__listItem"><a class="nav__listLink" href="#">Article</a></li>
+          <li class="nav__listItem" @click="modalStore.openModal('login')"><a class="nav__listLink" href="#">Sign in</a>
+          </li>
+          <li class="nav__listItem" @click="modalStore.openModal('register')"><a class="nav__listLink"
+              href="#">Register</a></li>
+        </ul>
+      </nav>
+</div>
+</template>
+
+<script setup>
+
+</script>
