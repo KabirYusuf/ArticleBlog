28b4679c45f254b24d2f25e99ad60cde7e1ed98d
Uyoobong Akpan <uyoobong.akpan@internetbrands.com>
2023-12-21 23:36:20 +0100

    Added google auth link

--- frontend/src/components/socials/SocialButton.vue
+++ frontend/src/components/socials/SocialButton.vue
@@ -2,13 +2,17 @@
     <div class="social__content">
         <small class="social__title">Sign In with: </small>
         <p><a href="#" class="social__button" id="gitlab__connect" @click="handleGitlabAuth"> <span>Gitlab</span></a></p>
-        <p><a href="#" class="social__button" id="google__connect"> <span> Google</span></a></p>
+        <p><a href="#" class="social__button" id="google__connect" @click="handleGoogleAuth"> <span> Google</span></a></p>
     </div>
 </template>
 
 <script setup>
 const handleGitlabAuth = () => {
     window.location.href = 'http://localhost:8080/oauth2/authorization/gitlab';
+};
+
+const handleGoogleAuth = () => {
+    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
 };
 
 </script>
